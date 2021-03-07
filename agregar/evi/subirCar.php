f3ff15f9c1e50b98b3e3fbbb3843a7ca3a12d071
OscarAbad04 <79182805+OscarAbad04@users.noreply.github.com>
2021-03-06 21:24:12 -0600

    Add files via upload

---
+++ agregar/evi/subirCar.php
@@ -0,0 +1,29 @@
+<?php 
+$msg = null;
+
+if (iseet($_POST["directorio"])) 
+{
+
+	$carpeta = htmlspecialchars($_POST["carpeta"]);
+	$ruta = htmlspecialchars($_POST["ruta"]);
+	$directorio = $ruta.$carpeta;
+
+	if (!is_dir($directorio)) 
+	{
+		$crear = mkdir($directorio, 0777, true);
+
+	if ($crear) 
+		{
+		$msg = "directorio creado correctamente";
+		}
+		else
+		{
+		$msg = "error al crear directorio";
+		}
+
+	}else{
+		$msg = "la carpeta ya existe";
+	}
+}
+
+ ?>
